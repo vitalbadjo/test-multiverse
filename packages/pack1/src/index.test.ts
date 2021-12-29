f76988cded1809b36dbe1e29f2ce727cb1876ae1
vitalbadjo <cueage@gmail.com>
2021-12-29 18:37:58 +0300

    fix: update jenkinsfile

--- packages/pack1/src/index.test.ts
+++ packages/pack1/src/index.test.ts
@@ -2,6 +2,6 @@ import { showHello } from "./index"
 
 describe("Hello test", () => {
 	test("Shold success say hello", () => {
-		expect(showHello()).toEqual("Hello Carl v3")
+		expect(showHello()).toEqual("Hello Carl v4")
 	})
 })
